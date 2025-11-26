6a756f4b33cd3c006b51f79fc5a86b3727963a2b
Quirky <48966267+Quirky1869@users.noreply.github.com>
2025-11-26 20:00:13 +0100

    Add trash-cli
    install_packets.sh

--- scripts/install_packets.sh
+++ scripts/install_packets.sh
@@ -11,5 +11,5 @@ sudo apt install -y tree wget netcat-traditional htop filezilla zip unzip rar un
     gnome-disk-utility gparted xournal netdiscover dirb hydra pluma bpytop edb-debugger pip \
     make gnome-shell-extensions rclone rclone-browser dsniff tcpdump libfuse2 pv curl network-manager \
     diodon bat john flameshot binwalk hashcat libimage-exiftool-perl baobab pipx asciinema tmux drawing micro \
-    freerdp2-x11 pinta hexcurse gnome-browser-connector ncdu
+    freerdp2-x11 pinta hexcurse gnome-browser-connector ncdu trash-cli
 
