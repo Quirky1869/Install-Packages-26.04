d9db0d237508949b892de510d2cf36f565593a37
Quirky <48966267+Quirky1869@users.noreply.github.com>
2025-11-28 17:45:49 +0100

    Modify install_dcv.sh to change installation path
    Comment out directory creation and ownership changes for /opt/dcv. Update the copy and symlink commands to target /usr/local/bin.

--- scripts/install_dcv.sh
+++ scripts/install_dcv.sh
@@ -8,12 +8,14 @@
 
 # Pour information : https://github.com/tokuhirom/dcv
 
-mkdir /opt/dcv
+#sudo mkdir /opt/dcv
 
-if ! find /opt -maxdepth 0 -user "$USER" -group "$USER" &>/dev/null || \
-   find /opt -not -user "$USER" -o -not -group "$USER" | grep -q .; then
-    sudo chown -R "$USER:$USER" /opt
-fi
+#if ! find /opt -maxdepth 0 -user "$USER" -group "$USER" &>/dev/null || \
+#   find /opt -not -user "$USER" -o -not -group "$USER" | grep -q .; then
+#    sudo chown -R "$USER:$USER" /opt
+#fi
 
-cp -rf ~/scripts/Necessary/dcv/dcv /opt/dcv/dcv
-sudo ln -s /opt/dcv/dcv /usr/local/bin/dcv
+#cp -rf ~/scripts/Necessary/dcv/dcv /opt/dcv/dcv
+#sudo ln -s /opt/dcv/dcv /usr/local/bin/dcv
+
+cp -rf ~/scripts/Necessary/dcv/dcv /usr/local/bin
